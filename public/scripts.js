3f81be5f32e8213b8c7f877e88041abb9953be66
LuizEduardoCardozo <eduard.cardoz@gmail.com>
2020-09-09 14:26:58 -0300

    release

---
+++ public/scripts.js
@@ -0,0 +1,57 @@
+const socket = io('/');
+const videoGrid = document.getElementById('video-grid');
+
+const peer = new Peer(undefined, {
+    host: '/',
+    port: (3001).toString(),
+})
+
+const video = document.createElement('video');
+video.muted = true;
+
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true,
+}).then(stream => {
+    addVideoStream(video, stream)
+
+    peer.on('call', (call) => {
+        call.answer();
+        const video = document.createElement('video');
+        call.on('stream', (userVideoStream) => {
+            addVideoStream(video, userVideoStream);
+        });
+        call.on('close', () => video.remove());
+
+    });
+
+
+    socket.on('user-conected', userId => {
+        connectToNewUser(userId, stream);
+    })
+
+});
+
+peer.on('open', (id) => {
+    socket.emit('join-room', ROOM_ID, id);
+});
+socket.emit('join-room', ROOM_ID, 101);
+
+function addVideoStream(video, stream) {
+    video.srcObject = stream;
+    video.addEventListener('loadedmetadata', () => {
+        video.play();
+    });
+    videoGrid.append(video);
+}
+
+function connectToNewUser(userId, stream) {
+    const call = peer.call(userId, stream);
+    const video = document.createElement('video');
+    call.on('stream', ( userVideoStream ) => {
+        addVideoStream(video, userVideoStream);
+    });
+    call.on('close', (  ) => {
+        video.remove()
+    })
+}
---
+++ src/index.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+const privateKey = fs.readFileSync('./localhost.key');
+const certificate = fs.readFileSync('./localhost.crt');
+
+const express = require('express');
+const app = express();
+const { static } = require('express');
+const server = require('https').createServer({key:privateKey,cert:certificate }, app);
+const io = require('socket.io')(server);
+const { v4 } = require('uuid');
+ 
+app.set('view engine', 'ejs');
+app.use(static('public'));
+
+app.get('/', ( req, res )=> res.redirect(`/${v4()}`) );
+
+app.get('/:room', ( req, res )=> res.render("room", { roomId: req.params.room }) );
+
+
+io.on('connection', (socket) => {
+
+    socket.on('join-room', (roomId, userId) => {
+
+        socket.broadcast.emit('user-conected', userId);
+
+    });
+
+});
+
+
+server.listen(3000);
